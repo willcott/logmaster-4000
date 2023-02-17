1b43eb3adfe8cc1af199d428409c5238648a0568
willcott <william.h.cottingham@gmail.com>
2023-02-17 10:25:52 +0000

    remove comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { putLog } from "./commands/putLog";
 import { updateLogs } from "./commands/updateLogs";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "log-extension" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let putDisposable = vscode.commands.registerCommand(
     "log-extension.putLog",
     putLog
@@ -28,5 +19,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(updateDisposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
